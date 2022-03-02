34ad10a1266850dab52878d0391431d294626b8d
digitalbricklayer <digitalbricklayer@gmail.com>
2022-03-02 11:22:09 +0000

    Adds CSP whitelist

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright (C) 2022 OPENXTRA Ltd. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register( 
+	  \Magento\Framework\Component\ComponentRegistrar::MODULE,
+	  'Openxtra_WarthogCsp',
+	  __DIR__
+	  );
